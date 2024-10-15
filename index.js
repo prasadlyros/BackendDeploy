4b1eb2a8dba074aa476c3505485df5069c8c234a
prasadlyros <prasad@lyrostech.com>
2024-10-15 16:45:52 +0530

    v1.0

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
 const express = require('express')
 const app = express()
-const data = require("./data/user")
-// app.get("/login", (req,res) => {
-    // res.writeHead(201, {"content-type" : "text/plain"})
-    // res.write("hello from server")
-    // res.end("end")
-    // res.status(201)
-    // res.statusCode = 201
-    // res.set({
-    //     "content-type":"text/plain",
-    //      "token":"nxjdhuyd"
-    // })
-    // res.status(201).send("hello from server")
-    // console.log("/login is hit")
-// })
+app.use(express.json())
 
-    app.get("/users",(req,res) => {
-        const query = req.query
-        console.log(query)
-        if(query.username){
-            const result=data.filter((item)=>item.username===query.username)
-            res.status(200).send(JSON.stringify(result))
-        }
-        else{
-            res.status(200).send(JSON.stringify(data))
-        }
-    })
+require('dotenv').config()
 
-app.listen(3004,() => console.log('server is started'))
+const cre = require('./routers/credentialRoute')
+app.use(cre)
+
+app.listen(process.env.DEV_PORT, () => console.log("Sever started"))
---
+++ models/credentialModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster0.jx4d7.mongodb.net/${process.env.TEST_DB}?retryWrites=true&w=majority&appName=Cluster0`
+const connect = mongoose.connect(url).then((res) => console.log("Connected successfully")).catch((err) => console.log(err))
+
+const credentialsSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:[true,"enter username it is require feild"]
+    },
+    password:{
+        type:String,
+        required  : true,
+        validate:{
+            validator : function(value){
+                return validator.isAscii(value)
+            },
+            message:"password validation failed, it should alpha numeric"
+        }
+    }
+})
+
+const credentialModel = mongoose.model("credentials",credentialsSchema)
+
+module.exports = {
+    credentialModel
+}
