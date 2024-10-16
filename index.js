f6c13bf6e1177206feaa40151b1799b69dc925dc
prasadlyros <prasad@lyrostech.com>
2024-10-16 14:17:10 +0530

    v1.2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json())
 require('dotenv').config()
 
 const cors = require('cors')
-app.use(cors({origin : "https://frontenddeployreact.netlify.app/",credentials:true}))
+app.use(cors({origin : "https://frontenddeployreact.netlify.app",credentials:true}))
 
 const cre = require('./routers/credentialRoute')
 app.use(cre)
